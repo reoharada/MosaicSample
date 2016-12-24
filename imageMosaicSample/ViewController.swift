5618dab14a0f79d2c68e54a0f1ba207a41f07b69
REO HARADA <reoharada@AppLab.local>
2016-12-24 23:31:19 +0900

    Initial Commit

---
+++ imageMosaicSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageMosaicSample
+//
+//  Created by REO HARADA on 2016/12/24.
+//  Copyright © 2016年 REO HARADA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ imageMosaicSampleTests/imageMosaicSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  imageMosaicSampleTests.swift
+//  imageMosaicSampleTests
+//
+//  Created by REO HARADA on 2016/12/24.
+//  Copyright © 2016年 REO HARADA. All rights reserved.
+//
+
+import XCTest
+@testable import imageMosaicSample
+
+class imageMosaicSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
